5e97444d12ba89a04a246153f70f006be10d9c78
SameenFatima <sameen.fatima993@gmail.com>
2025-08-06 11:45:43 +0500

    dayOneChallangeDone

---
+++ app.js
@@ -0,0 +1,11 @@
+//____****<_100_Days_Javascript_Challange_>******___//
+
+///////_______challange_1_
+
+var userName = prompt("Enter Your Name");
+var num1 = +prompt("Enter Any Number");
+var num2 = +prompt("Enter Any Number");
+
+var sumOfNumber = num1 + num2;
+
+alert(" Hello " + userName + " the sum of " + num1 + " and " + num2 + " is = " +  sumOfNumber)
