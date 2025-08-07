ae19854989e3349c9614009a7f2d8f61c38a9c11
SameenFatima <sameen.fatima993@gmail.com>
2025-08-07 13:00:39 +0500

    day2_task

---
+++ Day_2/app.js
@@ -0,0 +1,50 @@
+/////////_______Day_2___task__
+//// mera code 
+
+// var num1 = +prompt("Enter Any number")
+// if(num1 !== 2){
+//     alert("this is an od number")
+// }
+// if(num1 === 2){
+//     alert("this is an even number")
+// }
+
+// with correction
+
+// var num1 = +prompt("Enter any number");
+
+// if (num1 % 2 === 0) {
+//   alert("This is an even number");
+// }
+
+// if (num1 % 2 !== 0) {
+//   alert("This is an odd number");
+// }
+
+/////////_______Day_2___task2__
+
+// var trafficSignalColor = prompt("Enter a traffic light color (red, green ,yellow)")
+ 
+// if(trafficSignalColor === "red"){
+//     alert("please stop and wait")
+// }
+// if(trafficSignalColor === "yellow"){
+//     alert("Get ready, the traffic will move soon.")
+// }
+// if(trafficSignalColor === "green"){
+//     alert("You may go now, it’s safe.")
+// }
+
+/////////_______Day_2___task3__
+
+var userNum = +prompt("Enter Three digit code")
+
+
+
+if(userNum === 786){
+    alert("your code is correct")
+}
+
+// if(userNum === "786"){
+//     alert("your code is correct")
+// }
